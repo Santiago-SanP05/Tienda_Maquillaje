db8a4f3f9436bf7e3c84674ea2c7d39a1176a23d
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-10-11 13:14:55 -0500

    feat: :sparkles: se añadiron procediminetos y llamados
    se añadio las consultas de la 6 hasta la 10 con sus correctos llamados
    
    By @Kevinromero04

--- dql.sql
+++ dql.sql
@@ -18,7 +18,7 @@ CALL Listar_Producto_Especifico('Labial');
 SELECT p.Nombre_producto, p.Descripcion, p.Precio, p.Stock
 FROM Productos p
 JOIN Categorias c ON p.ID_categoria = c.ID_Categoria
-WHERE c.Categoria = 'Cosméticos'  -- Cambia esta categoría según lo necesites
+WHERE c.Categoria = 'Cosméticos' 
 AND p.Stock < 200; 
 
 
@@ -26,5 +26,104 @@ SELECT v.ID_Venta, v.Fecha_venta, c.Nombre_Completo, e.Nombre_Empleado
 FROM Ventas v
 JOIN Clientes c ON v.ID_cliente = c.ID_Cliente
 JOIN Empleados e ON v.ID_empleado = e.ID_Empleado
-WHERE c.Nombre_Completo = 'María García'  -- Cambia este nombre por el cliente específico
-AND v.Fecha_venta BETWEEN '2024-10-01' AND '2024-10-12';  -- Cambia el rango de fechas según lo necesites
+WHERE c.Nombre_Completo = 'María García' 
+AND v.Fecha_venta BETWEEN '2024-10-01' AND '2024-10-12';  
+
+-- 6. Consultar el stock disponible de un producto por su nombre o identificador.
+DELIMITER //
+CREATE PROCEDURE ConsultarStock(IN p_nombre_producto VARCHAR(100), IN p_id_producto INT)
+BEGIN
+    SELECT Nombre_producto, Stock
+    FROM Productos
+    WHERE (Nombre_producto = p_nombre_producto OR ID_Producto = p_id_producto)
+    AND (p_nombre_producto IS NOT NULL OR p_id_producto IS NOT NULL);
+END //
+
+DELIMITER ;
+
+-- 7. Mostrar las órdenes de compra realizadas a un proveedor específico en el último año.
+
+DELIMITER //
+
+CREATE PROCEDURE MostrarOrdenesPorProveedor(IN p_id_proveedor INT)
+BEGIN
+    SELECT *
+    FROM Orden_proveedores
+    WHERE ID_proveedor = p_id_proveedor
+    AND Fecha_Orden >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+END //
+
+DELIMITER ;
+
+
+
+-- 8. Listar los empleados que han trabajado más de un año en la tienda.
+
+
+DELIMITER //
+
+CREATE PROCEDURE ListarEmpleadosAntiguedad()
+BEGIN
+    SELECT *
+    FROM Empleados
+    WHERE Fecha_Contratacion <= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+END //
+
+DELIMITER ;
+
+
+-- 9. Obtener la cantidad total de productos vendidos en un día específico.
+
+
+DELIMITER //
+
+CREATE PROCEDURE CantidadTotalVendidaPorFecha(IN p_fecha DATE)
+BEGIN
+    SELECT SUM(Ventas_Productos.Cantidad) AS Total_Vendido
+    FROM Ventas_Productos
+    JOIN Ventas ON Ventas_Productos.ID_venta = Ventas.ID_Venta
+    WHERE Ventas.Fecha_venta = p_fecha;
+END //
+
+DELIMITER ;
+
+
+-- 10 Consultar las ventas de un producto específico (por nombre o ID) y cuántas unidades se vendieron.
+
+DELIMITER //
+
+CREATE PROCEDURE ConsultarVentasProducto(IN p_nombre_producto VARCHAR(100),IN p_id_producto INT)
+BEGIN
+    SELECT p.Nombre_producto, SUM(vp.Cantidad) AS Unidades_Vendidas
+    FROM Ventas_Productos vp
+    JOIN Productos p ON vp.ID_producto = p.ID_Producto
+    JOIN Ventas v ON vp.ID_venta = v.ID_Venta
+    WHERE (p.Nombre_producto = p_nombre_producto OR p.ID_producto = p_id_producto)
+    AND (p_nombre_producto IS NOT NULL OR p_id_producto IS NOT NULL)
+    GROUP BY p.Nombre_producto;
+END //
+
+DELIMITER ;
+
+-- LLAMADOS DE CONSULTAS
+
+--1.
+
+--2.
+
+--3.
+
+--4.
+
+--5.
+
+--6.
+CALL ConsultarStock('Labial Rouge', NULL);
+--7.
+CALL MostrarOrdenesPorProveedor(1);
+--8.
+CALL ListarEmpleadosAntiguedad();
+--9.
+CALL CantidadTotalVendidaPorFecha('2024-10-10');
+--10
+CALL ConsultarVentasProducto(NULL, 1);
